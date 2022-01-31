66abc5a9e7391162237e79d32681bc30e2bb29fb
toshick <toshickjazz@mac.com>
2022-01-31 23:33:53 +0900

    feat: firebase image upload (real buckget)

---
+++ packages/firebase-upload/script/upload.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs-extra');
+const { upload } = require('./helper');
+
+const imgpath = path.join(__dirname, '../tmp/dog2.png');
+const filename = path.basename(imgpath);
+// バケット上でのパス
+const distpath = `album/${filename}`;
+
+upload(imgpath, distpath).then((res) => {
+  if (res.error) {
+    console.log('エラー', res.error);
+    return;
+  }
+  console.log('アップロード成功', res);
+});
